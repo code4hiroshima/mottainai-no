923b6808c75029a148a5b1dd4f39f65d6cc5008e
14H-02390 <kou06ishi16@gmail.com>
2018-01-23 16:41:03 +0900

    あいあいネットさんの「食材リスト」へのリンクを削除 ＊打合せの結果反映

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ var cc1 = L.marker([34.392487, 132.475126], { icon: ccIcon }).bindPopup('<table 
     cc3 = L.marker([34.385509, 132.452631], { icon: ccIcon }).bindPopup('<table border="1"><tr><th>名称</th><td><a href="https://hiroshimaywca.jimdo.com/" target="_blank">わいわい食堂</a></td></tr><tr><th>案内</th><td><a href="https://maps.google.co.jp/maps?q=34.385509,132.452631&iwloc=J" target="_blank">ここまでの経路</a></td></tr></table>');
 
 //フードバンクのピンを追加 foodbank:fb
-var fb1 = L.marker([34.524403, 132.505580], { icon: fbIcon }).bindPopup('<table border="1"><tr><th>名称</th><td><a href="http://www.aiainet.org/" target="_blank">あいあいねっと</a></td></tr><tr><th>案内</th><td><a href="https://maps.google.co.jp/maps?q=34.524403,132.505580&iwloc=J" target="_blank">ここまでの経路</a></td></tr><tr><th>食材リスト</th><td><a href="http://mottainai-no.code4hiroshima.org/foodbank-foodlist1.html" target="_blank">コチラ</a></td></tr></table>');
+var fb1 = L.marker([34.524403, 132.505580], { icon: fbIcon }).bindPopup('<table border="1"><tr><th>名称</th><td><a href="http://www.aiainet.org/" target="_blank">あいあいねっと</a></td></tr><tr><th>案内</th><td><a href="https://maps.google.co.jp/maps?q=34.524403,132.505580&iwloc=J" target="_blank">ここまでの経路</a></td></tr></table>');
 
 //食品ロス削減協力店のピンを追加 lossnon:ln
 var ln1 = L.marker([34.362869,132.468328], { icon: lnIcon }).bindPopup('<table border="1"><tr><th>名称</th><td><a href="http://www.city.hiroshima.lg.jp/www/contents/1498549753659/index.html" target="_blank">イオン宇品店</a></td></tr><tr><th>案内</th><td><a href="https://maps.google.co.jp/maps?q=34.362869,132.468328&iwloc=J" target="_blank">ここまでの経路</a></td></tr></table>');
